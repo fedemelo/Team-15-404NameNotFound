70138540ea7dac4656b21b01ad9d5b2e7e6ed271
dburgos26 <d.burgos@uniandes.edu.co>
2024-09-27 20:41:44 -0500

    feat: add custom search bar

---
+++ unitrade/lib/pages/home/custom_search_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'package:unitrade/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomSearchBar extends StatefulWidget {
+
+  final void Function(String) onChange;
+
+  const CustomSearchBar(void Function(String) this.onChange,{super.key});
+
+  @override
+  _CustomSearchBarState createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        border:
+        Border.all(color: AppColors.primary900, width: 1.5),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: () {
+              print('search icon pressed ');
+            },
+            child: Icon(Icons.search, color: AppColors.primary900),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'What are you looking for?',
+                hintStyle: GoogleFonts.urbanist(
+                    fontSize: 15,
+                    color: AppColors.primary900,
+                    fontWeight: FontWeight.w600),
+                border: InputBorder.none,
+              ),
+              onChanged: widget.onChange,
+            ),
+          ),
+          SizedBox(width: 8),
+          GestureDetector(
+            onTap: () {
+              print('Filter icon pressed');
+            },
+            child: Icon(Icons.tune, color: AppColors.primary900),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- unitrade/lib/pages/home/home.dart
+++ unitrade/lib/pages/home/home.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:unitrade/pages/home/nav_bar.dart';
+import 'package:unitrade/pages/home/custom_search_bar.dart';
 
-class Home extends StatelessWidget {
+import 'package:unitrade/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  void searchValue(String value) {
+    print('Search value: $value');
+  }
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: ListView(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+
+                CustomSearchBar(
+                  widget.searchValue,
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                Text(
+                  'Categories',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      color: AppColors.primary900,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       bottomNavigationBar: const NavBar(),
     );
