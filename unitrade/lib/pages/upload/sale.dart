500b6e497e7149cbd4d331b152b157bb9d149994
Juan Sebastian <j.nunezc@uniandes.edu.co>
2024-09-30 19:24:13 -0500

    refactor: minor fixes regarding styles and validations

--- unitrade/lib/pages/upload/sale.dart
+++ unitrade/lib/pages/upload/sale.dart
@@ -17,10 +17,10 @@ class Sale extends StatefulWidget {
 class _SaleState extends State<Sale> {
   // Form data
   final _form = GlobalKey<FormState>();
-  String _name = '';
-  String _description = '';
-  String _price = '';
-  String _condition = '';
+  var _name = '';
+  var _description = '';
+  var _price = '';
+  var _condition = '';
 
   // Image data
   final ImagePicker _picker = ImagePicker();
@@ -81,8 +81,9 @@ class _SaleState extends State<Sale> {
   }
 
   // Upload product & image to Firebase
-  Future<void> _submit() async {
-    // TODO
+  void _submit() async {
+    _form.currentState!.save();
+
     // First - Validate form
     if (kDebugMode) {
       print('Name: $_name');
@@ -90,6 +91,11 @@ class _SaleState extends State<Sale> {
       print('Price: $_price');
       print('Condition: $_condition');
       print('Form: ${_form.currentState}');
+    }
+
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
     }
 
     // Second - Upload image to Firebase Storage
@@ -125,204 +131,221 @@ class _SaleState extends State<Sale> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(28.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Form(
+            key: _form,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
 
-            // TITLE
-            Text(
-              'Sale',
-              style: GoogleFonts.urbanist(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 16),
+                // TITLE
+                Text(
+                  'Sale',
+                  style: GoogleFonts.urbanist(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 16),
 
-            // NAME INPUT
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Name',
-                labelStyle: GoogleFonts.urbanist(
-                  fontSize: 16,
-                  color: const Color(0xFF6F6F6F),
-                  fontWeight: FontWeight.w400,
+                // NAME INPUT
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    labelStyle: GoogleFonts.urbanist(
+                      fontSize: 16,
+                      color: const Color(0xFF6F6F6F),
+                      fontWeight: FontWeight.w400,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
+                    ),
+                    errorStyle: GoogleFonts.urbanist(),
+                  ),
+                  style: GoogleFonts.urbanist(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a name for the product';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
                 ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter a name for the product';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _name = value!;
-              },
-            ),
-            const SizedBox(height: 28),
+                const SizedBox(height: 28),
 
-            // DESCRIPTION INPUT
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Description',
-                labelStyle: GoogleFonts.urbanist(
-                  fontSize: 16,
-                  color: const Color(0xFF6F6F6F),
-                  fontWeight: FontWeight.w400,
+                // DESCRIPTION INPUT
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    labelStyle: GoogleFonts.urbanist(
+                      fontSize: 16,
+                      color: const Color(0xFF6F6F6F),
+                      fontWeight: FontWeight.w400,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
+                    ),
+                    errorStyle: GoogleFonts.urbanist(),
+                  ),
+                  style: GoogleFonts.urbanist(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a description for the product';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _description = value!;
+                  },
                 ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter a description for the product';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _description = value!;
-              },
-            ),
-            const SizedBox(height: 28),
+                const SizedBox(height: 28),
 
-            // PRICE INPUT
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Price',
-                labelStyle: GoogleFonts.urbanist(
-                  fontSize: 16,
-                  color: const Color(0xFF6F6F6F),
-                  fontWeight: FontWeight.w400,
+                // PRICE INPUT
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Price',
+                    labelStyle: GoogleFonts.urbanist(
+                      fontSize: 16,
+                      color: const Color(0xFF6F6F6F),
+                      fontWeight: FontWeight.w400,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
+                    ),
+                    errorStyle: GoogleFonts.urbanist(),
+                  ),
+                  style: GoogleFonts.urbanist(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a price for the product';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _price = value!;
+                  },
                 ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter a price for the product';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _price = value!;
-              },
-            ),
-            const SizedBox(height: 28),
+                const SizedBox(height: 28),
 
-            // CONDITION INPUT
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Condition',
-                labelStyle: GoogleFonts.urbanist(
-                  fontSize: 16,
-                  color: const Color(0xFF6F6F6F),
-                  fontWeight: FontWeight.w400,
+                // CONDITION INPUT
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Condition',
+                    labelStyle: GoogleFonts.urbanist(
+                      fontSize: 16,
+                      color: const Color(0xFF6F6F6F),
+                      fontWeight: FontWeight.w400,
+                    ),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
+                    ),
+                    errorStyle: GoogleFonts.urbanist(),
+                  ),
+                  style: GoogleFonts.urbanist(),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter the condition of the product';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _condition = value!;
+                  },
                 ),
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Color.fromARGB(255, 178, 178, 178)),
-                ),
-              ),
-              validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return 'Please enter the condition of the product';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                _condition = value!;
-              },
-            ),
-            const SizedBox(height: 48),
+                const SizedBox(height: 48),
 
-            // IMAGE SELECTION & UPLOAD BUTTON
-            Center(
-              child: Column(
-                children: [
-                  // SELECT IMAGE BUTTON
-                  GestureDetector(
-                    onTap: () {
-                      _showPickerDialog(context);
-                    },
-                    child: Container(
-                      width: 600,
-                      padding: const EdgeInsets.all(40),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 237, 236, 236),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.image_search_rounded,
-                            size: 48,
-                            color: Colors.black,
+                // IMAGE SELECTION & UPLOAD BUTTON
+                Center(
+                  child: Column(
+                    children: [
+                      // SELECT IMAGE BUTTON
+                      GestureDetector(
+                        onTap: () {
+                          _showPickerDialog(context);
+                        },
+                        child: Container(
+                          width: 600,
+                          padding: const EdgeInsets.all(40),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 237, 236, 236),
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Select image',
-                            style: GoogleFonts.urbanist(
-                              textStyle: const TextStyle(
-                                fontSize: 16,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.image_search_rounded,
+                                size: 48,
                                 color: Colors.black,
                               ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 48),
-
-                  // UPLOAD BUTTON
-                  ElevatedButton(
-                    onPressed: _submit,
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all<Color>(
-                        AppColors.primary900,
-                      ),
-                      padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
-                        const EdgeInsets.symmetric(
-                            horizontal: 22, vertical: 18),
-                      ),
-                      foregroundColor: WidgetStateProperty.all<Color>(
-                        Colors.white,
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Icon(Icons.file_upload_outlined),
-                        const SizedBox(width: 14),
-                        Text(
-                          'UPLOAD PRODUCT',
-                          style: GoogleFonts.urbanist(
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Select image',
+                                style: GoogleFonts.urbanist(
+                                  textStyle: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 48),
+
+                      // UPLOAD BUTTON
+                      ElevatedButton(
+                        onPressed: _submit,
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            AppColors.primary900,
+                          ),
+                          padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                            const EdgeInsets.symmetric(
+                                horizontal: 22, vertical: 18),
+                          ),
+                          foregroundColor: WidgetStateProperty.all<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(Icons.file_upload_outlined),
+                            const SizedBox(width: 14),
+                            Text(
+                              'UPLOAD PRODUCT',
+                              style: GoogleFonts.urbanist(
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
