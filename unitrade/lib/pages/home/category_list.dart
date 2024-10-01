e0a97d2a544f0517c0444dfea479e23833682f6c
dburgos26 <d.burgos@uniandes.edu.co>
2024-10-01 00:47:02 -0500

    feat: add category item and list

---
+++ unitrade/lib/pages/home/category_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:unitrade/pages/home/category_item.dart';
+
+class CategoryList extends StatefulWidget {
+  final List<String> categories;
+  final String selectedCategory;
+  final Function(String) onClick;
+
+  CategoryList({
+    required this.categories,
+    required this.selectedCategory,
+    required this.onClick,
+  });
+
+  @override
+  _CategoryListState createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: widget.categories.map(
+          (category) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: CategoryItem(
+                icon: Icons.category,
+                title: category,
+                itemCount: 5,
+                selected: widget.selectedCategory == category,
+                onTap: widget.onClick,
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
--- unitrade/lib/pages/home/home.dart
+++ unitrade/lib/pages/home/home.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:unitrade/pages/home/category_list.dart';
 import 'package:unitrade/pages/home/nav_bar.dart';
 import 'package:unitrade/pages/home/custom_search_bar.dart';
 
 import 'package:unitrade/app_colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const categoryElementList = [
+  'For You',
+  'Category 1',
+  'Category 2',
+  'Category 3',
+  'Category 4',
+  'Category 5',
+];
+
+
 class Home extends StatefulWidget {
   const Home({super.key});
-
-  void searchValue(String value) {
-    print('Search value: $value');
-  }
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  var selectedCategory = 'For You';
+
+  void clickCategory(String category) {
+    print('Category: $category');
+    setState(() {
+      selectedCategory = category;
+    });
+  }
+
+  void searchValue(String value) {
+    print('Search value: $value');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +52,7 @@ class _HomeState extends State<Home> {
                 ),
 
                 CustomSearchBar(
-                  widget.searchValue,
+                  searchValue,
                 ),
 
                 const SizedBox(
@@ -45,6 +66,33 @@ class _HomeState extends State<Home> {
                       color: AppColors.primary900,
                       fontWeight: FontWeight.w600),
                 ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                CategoryList(
+                  categories: categoryElementList,
+                  selectedCategory: selectedCategory,
+                  onClick: clickCategory,
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                Text(
+                  selectedCategory,
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      color: AppColors.primary900,
+                      fontWeight: FontWeight.w600),
+                ),
+
+
+
+
+
               ],
             ),
           ),
