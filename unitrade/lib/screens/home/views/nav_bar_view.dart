c4820fc66ec2e2e96d10aad5f3d3b98f87931123
Juan Sebastian <j.nunezc@uniandes.edu.co>
2024-10-10 18:32:06 -0500

    refactor: Update NavBarView to use indexed navigation

--- unitrade/lib/screens/home/views/nav_bar_view.dart
+++ unitrade/lib/screens/home/views/nav_bar_view.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:unitrade/screens/home/views/home_view.dart';
 import 'package:unitrade/screens/upload/views/upload_path_view.dart';
 import 'package:unitrade/utils/app_colors.dart';
 
-class NavBarView extends StatelessWidget {
+class NavBarView extends StatefulWidget {
   const NavBarView({super.key});
+
+  @override
+  NavBarViewState createState() => NavBarViewState();
+}
+
+class NavBarViewState extends State<NavBarView> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Navigate to the corresponding view
+    switch (index) {
+      case 0:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeView()),
+        );
+        break;
+      case 1:
+        // Shopping cart logic
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const UploadPathView()),
+        );
+        break;
+      case 3:
+        // Notifications logic
+        break;
+      case 4:
+        // Profile logic
+        break;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,40 +54,43 @@ class NavBarView extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           IconButton(
-            icon: const Icon(Icons.home_outlined, color: AppColors.contrast900),
-            onPressed: () {
-              // Home logic
-            },
+            icon: Icon(
+              _selectedIndex == 0 ? Icons.home : Icons.home_outlined,
+              color: AppColors.contrast900,
+            ),
+            onPressed: () => _onItemTapped(0),
           ),
           IconButton(
-            icon: const Icon(Icons.shopping_cart_outlined,
-                color: AppColors.contrast900),
-            onPressed: () {
-              // Shopping cart logic
-            },
+            icon: Icon(
+              _selectedIndex == 1
+                  ? Icons.shopping_cart
+                  : Icons.shopping_cart_outlined,
+              color: AppColors.contrast900,
+            ),
+            onPressed: () => _onItemTapped(1),
           ),
           IconButton(
-            icon: const Icon(Icons.add_circle, color: AppColors.contrast900),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const UploadPathView()),
-              );
-            },
+            icon: Icon(
+              _selectedIndex == 2 ? Icons.add_circle : Icons.add_circle_outline,
+              color: AppColors.contrast900,
+            ),
+            onPressed: () => _onItemTapped(2),
           ),
           IconButton(
-            icon: const Icon(Icons.notifications_none_outlined,
-                color: AppColors.contrast900),
-            onPressed: () {
-              // Notifications logic
-            },
+            icon: Icon(
+              _selectedIndex == 3
+                  ? Icons.notifications
+                  : Icons.notifications_none_outlined,
+              color: AppColors.contrast900,
+            ),
+            onPressed: () => _onItemTapped(3),
           ),
           IconButton(
-            icon:
-                const Icon(Icons.person_outline, color: AppColors.contrast900),
-            onPressed: () {
-              // Profile logic
-            },
+            icon: Icon(
+              _selectedIndex == 4 ? Icons.person : Icons.person_outline,
+              color: AppColors.contrast900,
+            ),
+            onPressed: () => _onItemTapped(4),
           ),
         ],
       ),
