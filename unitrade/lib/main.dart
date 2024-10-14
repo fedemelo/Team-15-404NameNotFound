582c0c7bff4ddc031f834715de3ff46b9b67f888
Juan Sebastian <j.nunezc@uniandes.edu.co>
2024-10-14 15:02:31 -0500

    refactor: Update main.dart to lock orientation to portraitUp

--- unitrade/lib/main.dart
+++ unitrade/lib/main.dart
@@ -4,9 +4,13 @@ import 'package:unitrade/screens/login/views/loading_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:unitrade/utils/theme_provider.dart';
 import 'utils/firebase_options.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
