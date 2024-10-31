0731580a87afd3f3a931ba4a655d7d1aec2a95fd
Ernesto Perez <ec.perez@uniandes.edu.co>
2024-10-30 20:27:27 -0500

    feat: Implemented business question for user semester and major with local storage and eventual connectivity queue

--- unitrade/lib/screens/login/viewmodels/welcome_view_model.dart
+++ unitrade/lib/screens/login/viewmodels/welcome_view_model.dart
@@ -48,6 +48,7 @@ class WelcomeViewModel extends ChangeNotifier {
         } else {
           Navigator.pushReplacement(
             context,
+            // TODO: Change this to HomeView when finished developing
             MaterialPageRoute(builder: (context) => const ItempickerView()),
           );
         }
